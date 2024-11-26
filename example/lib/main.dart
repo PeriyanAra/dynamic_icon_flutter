02c80d0ad6e65760178a5e0288ce9746178994bd
Ara Periyan <periyan.ara@gmail.com>
2024-11-26 15:45:09 +0400

    fix example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: deprecated_member_use
 
+import 'dart:io';
+
 import 'package:dynamic_icon_flutter/dynamic_icon_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -14,9 +16,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final listAvailableIcon = <String>[
+    "nozomi",
+    "moonlight",
+    "ipsc",
+    "vanphuc",
+    "hadopm",
+    "default",
+    "MainActivity"
+  ];
+
   int batchIconNumber = 0;
 
-  String currentIconName = "?";
+  String currentIconName = "nozomi";
 
   bool loading = false;
 
@@ -26,11 +38,11 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
 
-    DynamicIconFlutter.getAlternateIconName().then((v) {
-      setState(() {
-        currentIconName = v ?? "`primary`";
-      });
-    });
+    // DynamicIconFlutter.getAlternateIconName().then((v) {
+    //   setState(() {
+    //     currentIconName = v ?? "nozomi`";
+    //   });
+    // });
   }
 
   @override
@@ -52,35 +64,37 @@ class _MyAppState extends State<MyApp> {
                 child: Text(
                   "Current Icon Name: $currentIconName",
                   textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headline4,
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
               ),
               OutlinedButton.icon(
                 icon: const Icon(Icons.ac_unit),
                 label: const Text("Team Fortress"),
                 onPressed: () async {
-                  try {
-                    //print(await DynamicIconFlutter.supportsAlternateIcons);
-                    if (await DynamicIconFlutter.supportsAlternateIcons) {
-                      await DynamicIconFlutter.setAlternateIconName(
-                          "teamfortress");
-                      _scaffoldKey.currentState?.hideCurrentSnackBar();
-                      _scaffoldKey.currentState?.showSnackBar(const SnackBar(
-                        content: Text("App icon change successful"),
-                      ));
-                      DynamicIconFlutter.getAlternateIconName().then((v) {
-                        setState(() {
-                          currentIconName = v ?? "`primary`";
-                        });
-                      });
-                      return;
+                  if (Platform.isAndroid) {
+                    Future.delayed(Duration(seconds: 2), () async {
+                      await DynamicIconFlutter.setIcon(
+                        icon: listAvailableIcon[2],
+                        listAvailableIcon: listAvailableIcon,
+                      );
+                    });
+                  } else {
+                    try {
+                      if (await DynamicIconFlutter.supportsAlternateIcons) {
+                        await DynamicIconFlutter.setAlternateIconName(
+                            listAvailableIcon[2]);
+                        print("App icon change successful");
+                        return;
+                      }
+                    } on PlatformException {
+                      if (await DynamicIconFlutter.supportsAlternateIcons) {
+                        await DynamicIconFlutter.setAlternateIconName(null);
+                        print("Change back to default app icon");
+                        return;
+                      } else {
+                        print("Failed to change app icon");
+                      }
                     }
-                  } on PlatformException {
-                    // ignore: avoid_print
-                    _scaffoldKey.currentState?.hideCurrentSnackBar();
-                    _scaffoldKey.currentState?.showSnackBar(const SnackBar(
-                      content: Text("Failed to change app icon"),
-                    ));
                   }
                 },
               ),
